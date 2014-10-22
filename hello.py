95ea31615c976b66cd2fc1a4a7e76b90bb19bdd2
PythoO <franck.lebrun@videotron.ca>
2014-10-22 00:06:06 -0400

    quick fix

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 import requests
 import urllib2
+import simplejson
 import json
 
 app = Flask(__name__)
