da59c88ce2a5b16c0c4faa0cf0dfbb54e1a5e610
PythoO <franck.lebrun@videotron.ca>
2014-10-21 22:22:15 -0400

    Add DB

--- hello.py
+++ hello.py
@@ -1,8 +1,37 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLACHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    isbn = db.Column(db.Integer)
+
+    def __init__(self, isbn):
+        self.isbn = isbn
+
+    def __repr__(self):
+        return '<TITLE %r>' % self.title
+
 
 @app.route('/')
-def hello():
-	return 'hello you!'
+def home():
+    return render_template("home.html")
+
+
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
+    if request.method == 'POST':
+        isbn = int(request.form['isbn'])
+        return redirect(url_for('home'))
+    return render_template("insert.html")
+
+
+@app.route('/delete/{id}')
+def delete():
+    return render_template("insert.html")
