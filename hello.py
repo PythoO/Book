37475e187b0289609e5043dcddb82a1b3e3fa9ad
PythoO <franck.lebrun@videotron.ca>
2014-10-22 23:24:23 -0400

    quick fix

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 import requests
 import urllib2
@@ -15,6 +15,7 @@ db = SQLAlchemy(app)
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
+    authors = db.Column(db.String(200))
     isbn = db.Column(db.Integer)
 
     def __init__(self, isbn):
@@ -37,19 +38,32 @@ def insert():
         google_url = 'https://www.googleapis.com/books/v1/volumes?q=%s+isbn' % isbn
         response = requests.get(google_url)
         html = response.text
+        #print html.encode('utf-8')
         try:
             data = json.loads(html)
         except Exception as e:
             return e.message
-        title = data['items']['title']
+        title = data['items'][0][u'volumeInfo'][u'title']
+        string_authors = ''
+        try:
+            authors = data['items'][0][u'volumeInfo'][u'authors']
+            for auth in authors:
+                string_authors += auth + ', '
+        except KeyError:
+            pass
         book = Book(isbn)
         book.title = title
+        book.authors = string_authors
         db.session.add(book)
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("insert.html")
 
 
-@app.route('/delete/{id}')
+@app.route('/delete', methods=['POST'])
 def delete():
-    return render_template("insert.html")
+    book_id = int(request.form['book_id'])
+    book = Book.query.filter_by(id=book_id).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('home'))
