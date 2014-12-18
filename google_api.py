fe160d4bf4bc48b96cedfc4b3435adf34818d26e
Franck Lebrun <flebrun@bkom.com>
2014-12-18 14:09:32 -0500

    Missing files

--- google_api.py
+++ google_api.py
@@ -1,1 +1,31 @@
-__author__ = 'flebrun'
+import requests
+import simplejson as json
+
+
+class GoogleApi():
+    isbn = ''
+    title = ''
+    description = ''
+    authors = ''
+    google_url = 'https://www.googleapis.com/books/v1/volumes?q=%s+isbn'
+
+    def __init__(self, isbn):
+        self.isbn = isbn
+
+    def get_data(self):
+        self.google_url = self.google_url % self.isbn
+        response = requests.get(self.google_url)
+        html = response.text
+        try:
+            data = json.loads(html)
+        except Exception as e:
+            return e.message
+
+        self.title = data['items'][0][u'volumeInfo'][u'title']
+        self.authors = ''
+        try:
+            authors = data['items'][0][u'volumeInfo'][u'authors']
+            for auth in authors:
+                self.authors += auth + ', '
+        except KeyError:
+            pass
--- models.py
+++ models.py
@@ -1,1 +1,47 @@
-__author__ = 'flebrun'
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    authors = db.Column(db.String(200))
+    isbn = db.Column(db.BigInteger)
+
+    def __init__(self, isbn):
+        self.isbn = isbn
+
+    def __repr__(self):
+        return '<TITLE %r>' % self.title
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(10))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
